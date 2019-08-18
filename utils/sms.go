3e627c4f9aac0e13f2cb16d95d78d1f972f7c33a
Raaj <raaj.ahab@gmail.com>
2019-08-18 17:51:47 +0000

    Changed HTTP call from https to http. There is an issue with calling https from inside the docker container

--- utils/sms.go
+++ utils/sms.go
@@ -4,7 +4,7 @@ package utils
 func SendOTPMessage(phone string, otp string, entity string) error {
 	message := "Welcome to " + entity + ". Your verification code is " + otp
 	_, sendMessageError := HttpClient.Get(
-		"https://apps.mnotify.net/smsapi?key=Fj369KxppKU0QJsngKBGdSNFS&to=" + phone + "&msg=" + message + "&sender_id=Polymorph")
+		"http://apps.mnotify.net/smsapi?key=Fj369KxppKU0QJsngKBGdSNFS&to=" + phone + "&msg=" + message + "&sender_id=Polymorph")
 
 	if sendMessageError != nil {
 		return sendMessageError
