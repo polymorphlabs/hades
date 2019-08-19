6d5764a9dcba7de566389b7557ca977aca3c5ad1
Raaj <raaj.ahab@gmail.com>
2019-08-19 11:42:20 +0000

    New otp generation strategy

--- utils/otp.go
+++ utils/otp.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+	"crypto/rand"
+	"math/big"
+	"strconv"
+)
+
+func GenerateOTP() (string, error){
+	nBig, e := rand.Int(rand.Reader, big.NewInt(89999))
+	if e != nil {
+		return "", e
+	}
+	return strconv.FormatInt(nBig.Int64()+10000, 10), nil
+}
